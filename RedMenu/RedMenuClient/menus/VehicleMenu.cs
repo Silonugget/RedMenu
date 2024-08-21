f5c7ffa7ee5be971695da1c7166ae15dfa271af8
Silonugget <107784929+Silonugget@users.noreply.github.com>
2024-08-20 23:34:03 -0600

    Update VehicleMenu.cs
    undoing making more stuff public

--- RedMenu/RedMenuClient/menus/VehicleMenu.cs
+++ RedMenu/RedMenuClient/menus/VehicleMenu.cs
@@ -32,7 +32,7 @@ namespace RedMenuClient.menus
 
     public class VehicleMenu : BaseScript
     {
-        public static Dictionary<string, JObject> vehicleConfigs = new Dictionary<string, JObject>();
+        private static Dictionary<string, JObject> vehicleConfigs = new Dictionary<string, JObject>();
 
         public VehicleMenu()
         {
@@ -40,7 +40,7 @@ namespace RedMenuClient.menus
             EventHandlers["receiveVehicleConfigJSON"] += new Action<string>(ReceiveVehicleConfig);
         }
 
-        public static void ReceiveVehicleConfig(string configJson)
+        private static void ReceiveVehicleConfig(string configJson)
         {
             // Parse the incoming JSON string into a JObject
             JObject configData = JObject.Parse(configJson);
